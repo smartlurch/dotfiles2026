c71b16f130ccc6f422c323bec911bceabedc1d0f
Eddie LeBlanc <eddie.leblanc@gmail.com>
2026-04-06 08:47:22 -0500

    tweaked bootstrap script

--- code/bootstrap/arch-bootstrap.sh
+++ code/bootstrap/arch-bootstrap.sh
@@ -1,3 +1,4 @@
 pacman -Syu
-pacman -S fuzzel niri cachyos-niri-settings cachyos-niri-noctalia noctalia cosmic-text-editor bazaar ente-auth-bin redshift wlsunset neovim nodejs npm unzip paru steam-devices tailscale trayscale gammastep
+pacman -S paru
+paru -S fuzzel niri cachyos-niri-noctalia noctalia cosmic-text-editor bazaar ente-auth-bin redshift wlsunset neovim nodejs npm steam-devices tailscale trayscale gammastep flatpak
 flatpak install com.bitwarden.desktop net.cozic.joplin.desktop org.onlyoffice.desktopeditors com.google.Chrome com.valvesoftware.steam dev.zed.Zed page.kramo.Cartridges io.ente.auth com.heroicgameslauncher.hgl
