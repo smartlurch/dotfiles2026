f9afc22318c1fb59b851668c7e81251bbcfcd138
Eddie LeBlanc <eddie.leblanc@gmail.com>
2026-04-06 09:47:17 -0500

    add flatpak config to bootstrap

--- code/bootstrap/arch-bootstrap.sh
+++ code/bootstrap/arch-bootstrap.sh
@@ -1,4 +1,5 @@
 pacman -Syu
 pacman -S paru
 paru -S fuzzel niri cachyos-niri-noctalia noctalia cosmic-text-editor bazaar ente-auth-bin redshift wlsunset neovim nodejs npm steam-devices tailscale trayscale gammastep flatpak
+flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install com.bitwarden.desktop net.cozic.joplin.desktop org.onlyoffice.desktopeditors com.google.Chrome com.valvesoftware.steam dev.zed.Zed page.kramo.Cartridges io.ente.auth com.heroicgameslauncher.hgl
