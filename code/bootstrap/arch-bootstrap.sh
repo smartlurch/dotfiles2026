ea960d75255f7505ecbdaf43df1c17e3345443c2
Eddie LeBlanc <eddie.leblanc@gmail.com>
2026-04-06 08:37:55 -0500

    add bootstrap script add nas to ssh config

---
+++ code/bootstrap/arch-bootstrap.sh
@@ -0,0 +1,3 @@
+pacman -Syu
+pacman -S fuzzel niri cachyos-niri-settings cachyos-niri-noctalia noctalia cosmic-text-editor bazaar ente-auth-bin redshift wlsunset neovim nodejs npm unzip paru steam-devices tailscale trayscale gammastep
+flatpak install com.bitwarden.desktop net.cozic.joplin.desktop org.onlyoffice.desktopeditors com.google.Chrome com.valvesoftware.steam dev.zed.Zed page.kramo.Cartridges io.ente.auth com.heroicgameslauncher.hgl
