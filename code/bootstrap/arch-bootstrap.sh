bab784ba750582a0d09fe98598efcc7852f49ceb
Eddie LeBlanc <eddie.leblanc@gmail.com>
2026-04-06 20:39:11 -0500

    working on bootstrap flatpak install and sddm

--- code/bootstrap/arch-bootstrap.sh
+++ code/bootstrap/arch-bootstrap.sh
@@ -1,5 +1,25 @@
 pacman -Syu
 pacman -S paru
-paru -S fuzzel niri cachyos-niri-noctalia noctalia cosmic-text-editor bazaar ente-auth-bin redshift wlsunset neovim nodejs npm steam-devices tailscale trayscale gammastep flatpak
-flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install com.bitwarden.desktop net.cozic.joplin.desktop org.onlyoffice.desktopeditors com.google.Chrome com.valvesoftware.steam dev.zed.Zed page.kramo.Cartridges io.ente.auth com.heroicgameslauncher.hgl
+paru -S sddm fuzzel niri cachyos-niri-noctalia noctalia cosmic-text-editor bazaar ente-auth-bin redshift wlsunset neovim nodejs npm steam-devices tailscale trayscale gammastep flatpak
+sudo systemctl enable -f sddm
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+APPS=(
+    "com.bitwarden.desktop"
+    "net.cozic.joplin.desktop"
+    "org.onlyoffice.desktopeditors"
+    "com.google.Chrome"
+    "com.valvesoftware.steam" 
+    "dev.zed.Zed" 
+    "page.kramo.Cartridges"
+    "io.ente.auth" 
+    "com.heroicgameslauncher.hgl"
+ )
+
+ # Install apps silently (-y assumes yes to all prompts)
+for app in "${APPS[@]}"; do
+    echo "Installing $app..."
+    flatpak install -y flathub "$app"
+done
+
